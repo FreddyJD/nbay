ce8594246e77f3eddd5fdb031aec82b7e191968c
Freddy <freddy.jd2@gmail.com>
2019-03-07 18:31:17 -0500

    Tindar added and fixed small bug in nbay .json file

---
+++ tindar/seed_data.js
@@ -0,0 +1,34 @@
+const friends = [
+    {
+       "name":"test",
+       "photo":"", // Pending phhotos -- with love freddy
+       "scores":[
+          "1",
+          "2",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1"
+       ]
+    },
+    {
+        "name":"test2",
+        "photo":"", // Pending phhotos -- with love freddy
+        "scores":[
+           "2",
+           "3",
+           "1",
+           "1",
+           "3",
+           "2",
+           "1",
+           "3",
+           "4",
+           "1"
+        ]
+     },
+]
